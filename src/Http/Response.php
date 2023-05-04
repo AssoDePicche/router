b0a46db1628f3e841bc44372667dde8d3c1a02cf
AssoDePicche <samuelprado730@gmail.com>
2023-05-04 00:01:35 -0300

    refactor: improve response class design

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -4,30 +4,23 @@ declare(strict_types=1);
 
 namespace Http;
 
-final class Response
+final readonly class Response
 {
-  public array $headers;
-
-  public function __construct(
-    public readonly mixed $content,
-    public readonly int $statusCode = 200,
-    public readonly string $contentType = 'text/html'
-  ) {
-    $this->headers['Content-Type'] = $contentType;
-  }
-
-  public function send(): never
-  {
-    http_response_code($this->statusCode);
-
-    foreach ($this->headers as $key => $value) {
-      header($key . ':' . $value);
+    public function __construct(
+        public string $content = '',
+        public int $statusCode = 200,
+        public array $headers = []
+    ) {
     }
 
-    switch ($this->contentType) {
-      case 'text/html':
+    public function send(): void
+    {
+        http_response_code($this->statusCode);
+
+        foreach ($this->headers as $key => $value) {
+            header($key . ':' . $value);
+        }
+
         echo $this->content;
-        exit;
     }
-  }
 }
