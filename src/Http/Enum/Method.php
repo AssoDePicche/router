0a807ad3f8704b504125b0d2c5245f1d4e47d8f4
AssoDePicche <samuelprado730@gmail.com>
2023-05-16 23:29:53 -0300

    feat: add more methods to method enum

--- src/Http/Enum/Method.php
+++ src/Http/Enum/Method.php
@@ -6,7 +6,13 @@ namespace Http\Enum;
 
 enum Method: string
 {
+    public const DELETE = 'DELETE';
+
     public const GET = 'GET';
 
+    public const PATCH = 'PATCH';
+
     public const POST = 'POST';
+
+    public const PUT = 'PUT';
 }
